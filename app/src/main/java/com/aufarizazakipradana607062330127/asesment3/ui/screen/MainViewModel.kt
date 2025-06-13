57984dc010f101cadaf5017e35817a6b82d0214a
Aufa Riza Zaki Pradana <aufa.rzp10@gmail.com>
2025-06-13 23:32:01 +0700

    Fix lagi

--- app/src/main/java/com/aufarizazakipradana607062330127/asesment3/ui/screen/MainViewModel.kt
+++ app/src/main/java/com/aufarizazakipradana607062330127/asesment3/ui/screen/MainViewModel.kt
@@ -2,7 +2,6 @@ package com.aufarizazakipradana607062330127.asesment3.ui.screen
 
 import android.graphics.Bitmap
 import android.util.Log
-import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
